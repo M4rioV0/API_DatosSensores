4935f78f58b9c7b96567c4691903ca9f0fd589b8
Mario Fernandez Fernandez <marioff196@gmail.com>
2022-06-13 12:22:50 +0200

    test iniciados

---
+++ API_prueba/DatosSensores/test/test_urls.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+class TestUrls(SimpleTestCase):
+
+    def test_advice_url_is_resolved(self):
+            assert 1 == 2 
